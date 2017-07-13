46a341d2f582e333a85869a58917cd915dbc151b
Walter Kuppens <reshurum@gmail.com>
2017-07-13 14:37:38 -0700

    Fixed mistakes in installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PLUGIN_URL = "https://raw.githubusercontent.com/StickmanVentures/ghost-gcs/master/index.js"
-PLUGIN_PATH = "content/storage/gcs"
+PLUGIN_URL="https://raw.githubusercontent.com/StickmanVentures/ghost-gcs/master/index.js"
+PLUGIN_PATH="content/storage/gcs"
 
 # Do a stupid check to see if we're in a ghost project directory and create the
 # directory structure to place the plugin in.
@@ -9,19 +9,21 @@ if [ ! -d "./content" ]; then
     (>&2 echo "Cannot find content directory, are you in your project root?")
     exit 1
 fi
-mkdir -p $(PLUGIN_PATH)
+mkdir -p $PLUGIN_PATH
 
 # Download the plugin from the GitHub master branch and install to content.
 if ! hash curl 2>/dev/null; then
     (>&2 echo "Curl is required to download the plugin.")
     exit 1
 fi
-if ! curl -o "$PLUGIN_PATH/index.js" $(PLUGIN_URL); then
+if ! curl -o "$PLUGIN_PATH/index.js" $PLUGIN_URL; then
     (>&2 echo "Failed to download plugin from GitHub, check your network.")
     exit 1
 fi
 
-echo "The plugin is now installed in your Ghost content directory.
+echo "===============================================================================
+
+The plugin is now installed in your Ghost content directory.
 To setup the plugin, add a storage key to your configuration.
 
 Example config:
